d35ccdb80b5b8e0582e49c544e193096ea6ad6d8
inJAJA <zaiin4050@gmail.com>
2020-11-23 14:59:51 +0900

    padding-conv

--- c/c15_padding.c
+++ c/c15_padding.c
@@ -2,71 +2,101 @@
 #include <stdlib.h>
 
 int main()
-{
-    int *x = (int*)malloc(9 * sizeof(int));
-    int *c = (int*)malloc(4 * sizeof(int));
+{   
+    // TensorFlow
+    // 먼저 padding 후 Conv계산
+    // 오른쪽 상단부터 padding
+
+    // Conv layer output_size = ( W - F + 2*P) / S + 1
+    // W : image_width
+    // F : Filter width
+    // P : Padding size
+    // S : Stride number
+
+    int *x = (int*)calloc(4 * 4 , sizeof(int)); // padding 후 conv계산
+    int *k = (int*)malloc(2* 2 * sizeof(int));
     int *b = (int*)malloc(2 * sizeof(int));
 
-    int *o = (int*)calloc(9, sizeof(int));
+    int *o = (int*)malloc(3 * 3 * sizeof(int)); // calloc : 메모리를 할당하고 0으로 초기화
 
-    printf("\no : ");                   // calloc : 메모리를 할당하고 0으로 초기화
+    printf("\no : ");                   
     for (int i = 0; i < 9; i++){
         printf("%d\t", o[i]);
     }
 
     int num[9] = {1, 2, 3, 5, 3, 2, 2, 6, 3};
-    int w[4] = {1, 0, 0, 1};
+    int weight[4] = {1, 0, 0, 1};
     b[0] = 1;   // => *b = 1;
     b[1] = 3;   // => *(b+1) = 3;
 
-    printf("\ninput x : ");
-    for (int i = 0; i < 9; i++){
-        x[i] = num[i];
-        printf("%d\t", x[i]);
+
+    for (int h = 0;  h < 3; h++){      // input x 
+        for (int w = 0; w < 3; w++){
+            x[(h+1)*4 + (w+1)] = num[h*3 + w];
+        }
     }
 
-    printf("\nweight : ");
-    for (int i = 0; i < 4; i++){
-        c[i] = w[i];
-        printf("%d\t", c[i]);
+    // Padding
+    printf("\ninput x\n");              // check input 'x'
+    for (int ph = 0; ph < 4; ph++){
+        for (int pw = 0; pw < 4; pw++){
+            printf("%d\t", x[ph*4 + pw]);
+        }
+        printf("\n");
+    }
+
+    printf("\nkernel\n");
+    for (int kh = 0; kh < 2; kh++){
+        for (int kw = 0; kw < 2; kw++){
+            k[kh*2 + kw] = weight[kh*2 + kw];
+            printf("%d\t", k[kh*2 + kw]);
+        }
+        printf("\n");
     }
 
     printf("\nbias : %d\n", *b);
     
-    for (int h = 0; h < 2; h++){
-        for (int w = 0; w < 2; w++){
+    // Conv
+    for (int p = 0; p < 3; p++){            // output image height
+        for (int q = 0; q < 3; q++){        // output image width
             int s = 0;
             for (int kh = 0; kh < 2; kh++){
                 for (int kw = 0; kw < 2; kw++){
-                    s += x[3*(h+kh)+w+kw]*c[2*kh+kw]; // 3 = input image_size, 2 = kernel_size
+                    s += x[(p+kh)*4 + q+kw] * k[kh*2 + kw]; // 4 = input image_size, 2 = kernel_size
                 }
             }
-            o[3*h+w] = s;       // 3 = output image_size
+            o[p*3 + q] = s;       // 3 = output image_size
         }
     }
 
-    printf("\nresult : ");
-    for (int i = 0; i < 9; i++){
-        printf("%d\t", o[i]);
+    printf("\nresult\n");
+    for (int p = 0; p < 3; p++){
+        for (int q = 0; q < 3; q++){
+            printf("%d\t", o[p*3 + q]);
+        }
+        printf("\n");
     }
 
 
     // bias
-    for (int h = 0; h < 2; h++){
-        for (int w = 0; w < 2; w++){
-            o[3*h+w] += b[0];
+    for (int p = 0; p < 3; p++){
+        for (int q = 0; q < 3; q++){
+            o[p*3 + q] += b[0];
         }
     }
 
-    printf("\nresult with bias :");
-    for (int i = 0; i < 9; i++){
-        printf("%d\t", o[i]);
+    printf("\nresult with bias\n");
+    for (int p = 0; p < 3; p++){
+        for (int q = 0; q < 3; q++){
+            printf("%d\t", o[p*3 + q]);
+        }
+        printf("\n");
     }
 
     
     // ᄁᆃᆨ 해줘야 한다!!!!!(습관 들이기)
    free(x);
-   free(c);
+   free(k);
    free(b);
    free(o);
 
--- c/c16_padding.c
+++ c/c16_padding.c
@@ -2,69 +2,97 @@
 #include <stdlib.h>
 
 int main()
-{
-    int *x = (int*)malloc(25 * sizeof(int));
-    int *c = (int*)malloc(9 * sizeof(int));
+{   
+    // TensorFlow
+    // 먼저 padding 후 Conv계산
+    // 오른쪽 상단부터 padding
+
+    // Conv layer output_size = ( W - F + 2*P) / S + 1
+    // W : image_width
+    // F : Filter width
+    // P : Padding size
+    // S : Stride number
+
+    int *x = (int*)calloc(7 * 7, sizeof(int));
+    int *k = (int*)malloc(3 * 3 *sizeof(int));
     int *b = (int*)malloc(1 * sizeof(int));
 
-    int *o = (int*)calloc(25, sizeof(int));
+    int *o = (int*)malloc(5 * 5 *sizeof(int)); // calloc : 메모리를 할당하고 0으로 초기화
 
-    printf("\no : ");                   // calloc : 메모리를 할당하고 0으로 초기화
-    for (int i = 0; i < 9; i++){
-        printf("%d\t", o[i]);
-    }
-
-    int num[25] = {1, 2, 3, 5, 3, 2, 2, 6, 3, 1, 2, 3, 5, 3, 2, 2, 6, 3, 1, 2, 3, 5, 3, 2, 2};
-    int w[9] = {1, 0, 1, 0, 1, 1, 0, 0, 1};
+    int num[5 * 5] = {1, 2, 3, 5, 3, 2, 2, 6, 3, 1, 2, 3, 5, 3, 2, 2, 6, 3, 1, 2, 3, 5, 3, 2, 2};
+    int weight[3 * 3] = {1, 0, 1, 0, 1, 1, 0, 0, 1};
     b[0] = 1;   // => *b = 1;
 
-    printf("\ninput x : ");
-    for (int i = 0; i < 25; i++){
-        x[i] = num[i];
-        printf("%d\t", x[i]);
+
+    // check num
+    printf("num\n");
+    for (int h = 0; h < 5; h++){
+        for (int w = 0; w < 5; w++){
+            printf("%d\t", num[h*5 +w]);
+        }
+        printf("\n");
     }
 
-    printf("\nweight : ");
-    for (int i = 0; i < 9; i++){
-        c[i] = w[i];
-        printf("%d\t", c[i]);
+    // Padding
+    for (int h = 0; h < 5; h++){
+        for (int w = 0; w < 5; w++){
+            x[(h+1)*7 + (w+1)] = num[h*5 + w];
+        }
     }
 
+    // check input x
+    printf("\ninput x\n");
+    for (int ph = 0; ph < 7; ph++){         // ph : padding 후 height
+        for (int pw = 0; pw < 7; pw++){     // pw : padding 후 widht
+            printf("%d\t", x[ph*7 + pw]);
+        }
+        printf("\n");
+    }
+   
+    printf("\nweight\n");
+    for (int kh = 0; kh < 3; kh++){
+        for (int kw = 0; kw < 3; kw++){
+            k[kh*3 + kw] = weight[kh*3 + kw];
+            printf("%d\t", k[kh*3 + kw]);
+        }   
+        printf("\n");
+    }
+    
     printf("\nbias : %d\n", *b);
     
-    // conv_padding
-    for (int h = 0; h < 3; h++){                       //  3 = feature_size
-        for (int w = 0; w < 3; w++){
+    // Conv
+    for (int p = 0; p < 5; p++){                        //  p = output image height
+        for (int q = 0; q < 5; q++){                    //  q = output image width
             int s = 0;
             for (int kh = 0; kh < 3; kh++){
                 for (int kw = 0; kw < 3; kw++){
-                    s += x[5*(h+kh)+w+kw]*c[3*kh+kw]; // 3 = input image_size, 2 = kernel_size
+                    s += x[(p+kh)*7 + q+kw]  *k[kh*3 + kw]; // 7 = input image_size, 3 = kernel_size
                 }
             }
-            o[5*(h+1)+w+1] = s;       // 5 = output image_size, 1 = padding
+            o[p*5 + q] = s;       // 5 = output image_size
         }
     }
 
     printf("\nresult\n");
-    for (int h = 0; h < 5; h++){
-        for (int w = 0; w < 5; w++){
-            printf("%d\t", o[5*h+w]);
+    for (int p = 0; p < 5; p++){
+        for (int q = 0; q < 5; q++){
+            printf("%d\t", o[p*5 +q]);
         }
         printf("\n");
     }
 
 
     // bias
-    for (int h = 0; h < 3; h++){
-        for (int w = 0; w < 3; w++){
-            o[5*(h+1)+w+1] += b[0];
+    for (int p = 0; p < 5; p++){
+        for (int q = 0; q < 5; q++){
+            o[p*5 + q] += b[0];
         }
     }
 
     printf("\nresult with bias\n");
-    for (int h = 0; h < 5; h++){
-        for (int w = 0; w < 5; w++){
-            printf("%d\t", o[5*h+w]);
+    for (int p = 0; p < 5; p++){
+        for (int q = 0; q < 5; q++){
+            printf("%d\t", o[p*5 + q]);
         }
         printf("\n");
     }
@@ -72,9 +100,8 @@ int main()
     
     // ᄁᆃᆨ 해줘야 한다!!!!!(습관 들이기)
    free(x);
-   free(c);
+   free(k);
    free(b);
    free(o);
-
-
+    
 }
